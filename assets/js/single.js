177b9affd0b1a2548ed6ccef4d87a24f6f97c734
mkanczuzewski <mkanczuzewski@gmail.com>
2021-11-02 11:41:21 -0400

    add link into issue on github and add logic for more then 30 results

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -7,10 +8,14 @@ var getRepoIssues = function(repo) {
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
-          response.json().then(function(data) {
-            // pass response data to dom function
-            displayIssues(data);
-          });
+            response.json().then(function(data) {
+                // pass response data to dom function
+                displayIssues(data);
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
+            });
         }
         else {
           alert("There was a problem with your request!");
@@ -23,7 +28,7 @@ var displayIssues = function(issues) {
         issueContainerEl.textContent = "This repo has no open issues!";
         return;
     }
-    
+
     for (var i = 0; i < issues.length; i++) {
         // create a link element to take users to the issue on github
         var issueEl = document.createElement("a");
@@ -55,3 +60,15 @@ var displayIssues = function(issues) {
 };
 
   getRepoIssues("facebook/react");
+  
+  var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
